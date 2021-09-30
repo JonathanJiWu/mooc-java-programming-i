3d2190805b6b8f4d0ef0f43acd21c0f8b39427fc
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-09-29 20:49:28 -0400

    new codes & more variable practice

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,10 +1,14 @@
-
+import java.util.Scanner;
 public class Sandbox {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Write your program here
-        System.out.println("Once upon a time");
-        System.out.println("there was");
-        System.out.println("a program");
+        System.out.println("Please enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        int num2 = scanner.nextInt();
+        int num3 = num1 + num2;
+        System.out.println("The sum of the two numbers is "+num3);
     }
 }
