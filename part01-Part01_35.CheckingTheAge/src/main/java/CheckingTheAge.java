dd74efc8a531cfd6971cf6fd17c98df05c3a40d1
JJW <jonathanwubrave@gmail.com>
2021-10-16 18:58:45 -0400

    tax calculator, leap years

--- part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -5,6 +5,12 @@ public class CheckingTheAge {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.print("How old are you?");
+        int input = scan.nextInt();
+        if (input<0 || input> 120){
+            System.out.println("Impossible!");
+        } else {
+            System.out.println("OK");
+        }
     }
 }
--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -5,6 +5,20 @@ public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Value of the gift?");
+        int input = scan.nextInt();
+        if (input < 5000) {
+            System.out.println("No tax!");
+        } else if (input < 25000) {
+            System.out.println("Tax: " + (100 + (input - 5000) * .08));
+        } else if (input < 55000) {
+            System.out.println("Tax: " + (1700 + (input - 25000) * .1));
+        } else if (input < 200000) {
+            System.out.println("Tax: " + (4700 + (input - 55000) * .12));
+        } else if (input < 1000000) {
+            System.out.println("Tax: " + (22100 + (input - 200000) * .15));
+        } else {
+            System.out.println("Tax: " + (142100 + (input - 1000000) * .17));
+        }
     }
 }
