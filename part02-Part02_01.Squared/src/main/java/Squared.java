b6603440ba06af963cbbafcf1ff00db928250103
JJW <jonathanwubrave@gmail.com>
2021-10-17 22:07:58 -0400

    Update Squared.java

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,8 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(i*i);
 
     }
 }
