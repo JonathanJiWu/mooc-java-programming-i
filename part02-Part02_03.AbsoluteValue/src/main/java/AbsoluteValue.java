b73f22b089f31fcfa21d902a017f177bf12d2856
JJW <jonathanwubrave@gmail.com>
2021-10-19 22:28:10 -0400

    absolute value and number comparison logics

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,11 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int n1 = scanner.nextInt();
+        if (n1<0){
+            System.out.println(n1*-1);
+        } else {
+            System.out.println(n1);
+        }
     }
 }
